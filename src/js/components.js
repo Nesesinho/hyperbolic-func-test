5a7c025e229b3a880335aef7213a5084bdb7e455
Anthony Passos <anthony.passos@sempreceub.com>
2024-04-24 00:53:33 -0300

    trabalhando a pagina inicial

---
+++ src/js/components.js
@@ -0,0 +1,9 @@
+const components = {
+    start: /*html*/`
+        <section class="bg-violet-600 w-2/6 h-5/6 rounded shadow-lg shadow-black p-5 flex items-center flex-col">
+            <h1 class="text-3xl">Qual função Hiperbolica você é?</h1>
+            <p>Descubra qual dos 8 tipos de funções hiperbolicas você é!!</p>
+            <button class="m-auto text-2xl bg-white w-60 h-20 rounded shadow-md shadow-black">Iniciar Teste</button>
+        </section>
+    `
+}
