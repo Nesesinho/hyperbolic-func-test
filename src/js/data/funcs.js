e890c244adc7cc17faf4b61d3c23f0bc7fcf46d0
Anthony Passos <anthony.passos@sempreceub.com>
2024-04-24 23:38:00 -0300

    adicionando as funções

---
+++ src/js/data/funcs.js
@@ -0,0 +1,50 @@
+export const funcs = [
+    {
+        name: "Tangente Hiperbólica",
+        type: "Idealista Empolgado",
+        points: 14,
+        desc: "Essa personalidade é apaixonada, imaginativa e sempre busca o lado mais otimista das situações. Assim como a tangente hiperbólica se estende ao infinito, eles estão constantemente buscando novas ideias e possibilidades."
+    },
+    {
+        name: "Cosseno Hiperbólico",
+        type: "Realista Estável",
+        points: 21,
+        desc: "Esta personalidade é equilibrada, pragmática e tem uma abordagem estável para a vida. Como o cosseno hiperbólico que se mantém entre os extremos, eles preferem manter as coisas em um nível mais consistente e previsível."
+    },
+    {
+        name: "Secante Hiperbólica",
+        type: "Analítico Reflexivo",
+        points: 28,
+        desc: "Essa personalidade é profunda, meticulosa e sempre busca entender as coisas em um nível mais profundo. Semelhante à secante hiperbólica que é o inverso da cosseno hiperbólica, eles estão constantemente desvendando os detalhes e nuances das situações."
+    },
+    {
+        name: "Cosecante Hiperbólica",
+        type: "Aventuroso Impulsivo",
+        points: 35,
+        desc: "Este tipo de personalidade é espontâneo, impulsivo e está sempre em busca de novas experiências. Como a cosecante hiperbólica que é o inverso da seno hiperbólica, eles tendem a se mover em direções inesperadas e não convencionais."
+    },
+    {
+        name: "Seno Hiperbólico",
+        type: "Ponderado Adaptável",
+        points: 42,
+        desc: "Essa personalidade é flexível, adaptável e sabe como se ajustar a diferentes situações. Semelhante ao seno hiperbólico que oscila entre os extremos, eles são capazes de se mover suavemente entre diferentes abordagens e perspectivas."
+    },
+    {
+        name: "Tangente Inversa Hiperbólica",
+        type: "Carismático Cativante",
+        points: 49,
+        desc: "Esta personalidade é carismática, persuasiva e tem uma capacidade única de atrair os outros para seu ponto de vista. Como a tangente inversa hiperbólica que descreve o crescimento lento e depois rápido, eles são capazes de criar um impacto duradouro com sua presença magnética."
+    },
+    {
+        name: "Cosseno Inverso Hiperbólico",
+        type: "Pragmático Determinado",
+        points: 56,
+        desc: "Esse tipo de personalidade é prático, determinado e focado em alcançar seus objetivos. Assim como o cosseno inverso hiperbólico que descreve um crescimento inicial rápido seguido de uma desaceleração, eles têm uma abordagem metódica e calculada para alcançar o sucesso."
+    },
+    {
+        name: "Secante Inversa Hiperbólica",
+        type: "Harmonioso Conciliador",
+        points: 64,
+        desc: "Essa personalidade é pacífica, diplomática e sempre busca o equilíbrio e a harmonia nas relações. Como a secante inversa hiperbólica que descreve um crescimento inicial lento seguido de um aumento rápido, eles são capazes de encontrar soluções criativas e satisfatórias para conflitos e desafios interpessoais."
+    }
+]
