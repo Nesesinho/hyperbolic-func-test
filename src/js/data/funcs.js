2438e567254bd130361836b684314a94fb4b69f2
Anthony Passos <anthony.passos@sempreceub.com>
2024-04-24 23:46:42 -0300

    adicionando personagens

--- src/js/data/funcs.js
+++ src/js/data/funcs.js
@@ -3,48 +3,184 @@ export const funcs = [
         name: "Tangente Hiperbólica",
         type: "Idealista Empolgado",
         points: 14,
+        characters: [
+            {
+              name: "Bob Esponja",
+              origin: "Bob Esponja Calça Quadrada",
+              imgUrl: "https://almeidajunior-prod1.s3.amazonaws.com/prod/uploads/news/5d7a7cbf6ad5d.jpg"
+            },
+            {
+              name: "Michael Scott",
+              origin: "The Office",
+              imgUrl: "https://tm.ibxk.com.br/2021/11/18/18150819447415.jpg"
+            },
+            {
+              name: "Phoebe Buffay",
+              origin: "Friends",
+              imgUrl: "https://upload.wikimedia.org/wikipedia/en/f/f6/Friendsphoebe.jpg"
+            }
+        ],
         desc: "Essa personalidade é apaixonada, imaginativa e sempre busca o lado mais otimista das situações. Assim como a tangente hiperbólica se estende ao infinito, eles estão constantemente buscando novas ideias e possibilidades."
     },
     {
         name: "Cosseno Hiperbólico",
         type: "Realista Estável",
         points: 21,
+        characters: [
+            {
+              name: "Hermione Granger",
+              origin: "Harry Potter",
+              imgUrl: "https://qph.cf2.quoracdn.net/main-qimg-1d13fb444d6cfbc88f6fae0acb630c7c-lq"
+            },
+            {
+              name: "Tyrion Lannister",
+              origin: "Game of Thrones",
+              imgUrl: "https://static.wikia.nocookie.net/gameofthrones/images/9/95/HandoftheKingTyrionLannister.PNG/revision/latest?cb=20190520175204"
+            },
+            {
+              name: "Samwise Gamgee",
+              origin: "Senhor dos Anéis",
+              imgUrl: "https://static.wikia.nocookie.net/lotr/images/5/52/Samwise_Gamgee_1.PNG/revision/latest?cb=20200617172124"
+            }
+        ],
         desc: "Esta personalidade é equilibrada, pragmática e tem uma abordagem estável para a vida. Como o cosseno hiperbólico que se mantém entre os extremos, eles preferem manter as coisas em um nível mais consistente e previsível."
     },
     {
         name: "Secante Hiperbólica",
         type: "Analítico Reflexivo",
         points: 28,
+        characters: [
+            {
+              name: "Sherlock Holmes",
+              origin: "Sherlock Holmes",
+              imgUrl: "https://s2.glbimg.com/KpzbDutodg_CHzXGzvvN79974fw=/e.glbimg.com/og/ed/f/original/2017/01/02/benedict-cumberbatch_destaque.jpg"
+            },
+            {
+              name: "Dr. Gregory House",
+              origin: "House",
+              imgUrl: "https://qph.cf2.quoracdn.net/main-qimg-662d308a60ea129d17b5ee2259d41fc8-lq"
+            },
+            {
+              name: "Sheldon Cooper",
+              origin: "The Big Bang Theory",
+              imgUrl: "https://i.pinimg.com/736x/10/18/55/101855f519ea89fc7e9a965a346a196f.jpg"
+            }
+          ],
         desc: "Essa personalidade é profunda, meticulosa e sempre busca entender as coisas em um nível mais profundo. Semelhante à secante hiperbólica que é o inverso da cosseno hiperbólica, eles estão constantemente desvendando os detalhes e nuances das situações."
     },
     {
         name: "Cosecante Hiperbólica",
         type: "Aventuroso Impulsivo",
         points: 35,
+        characters: [
+            {
+              name: "Jack Sparrow",
+              origin: "Piratas do Caribe",
+              imgUrl: "https://static.wikia.nocookie.net/liberproeliis/images/8/8a/Jack_Sparrow.jpg/revision/latest?cb=20200613004620&path-prefix=pt-br"
+            },
+            {
+              name: "Deadpool",
+              origin: "Deadpool",
+              imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsx8XwgyZrg6RRh_kWwi0Drzm5Qk_wL7kj4CTACPTE3A&s"
+            },
+            {
+              name: "Jake Peralta",
+              origin: "Brooklyn Nine-Nine",
+              imgUrl: "https://i.pinimg.com/originals/9e/dc/78/9edc78d8a8414a7b2f5904fcff891dea.jpg"
+            }
+        ],
         desc: "Este tipo de personalidade é espontâneo, impulsivo e está sempre em busca de novas experiências. Como a cosecante hiperbólica que é o inverso da seno hiperbólica, eles tendem a se mover em direções inesperadas e não convencionais."
     },
     {
         name: "Seno Hiperbólico",
         type: "Ponderado Adaptável",
         points: 42,
+        characters: [
+            {
+              name: "Capitão América",
+              origin: "Marvel ",
+              imgUrl: "https://rollingstone.uol.com.br/media/_versions/chris_evans__-capitao_america_-_reproducao_widelg.jpg"
+            },
+            {
+              name: "Daenerys Targaryen",
+              origin: "Game of Thrones",
+              imgUrl: "https://static.wikia.nocookie.net/gameofthrones/images/b/b6/DanyS8E6.jpg/revision/latest?cb=20220110160915&path-prefix=pt-br"
+            },
+            {
+              name: "Luke Skywalker",
+              origin: "Star Wars",
+              imgUrl: "https://uploads.jovemnerd.com.br/wp-content/uploads/2020/06/luke-skywalker-jedi-professor-mark-hamill.jpg?ims=filters:quality(75)"
+            }
+        ],
         desc: "Essa personalidade é flexível, adaptável e sabe como se ajustar a diferentes situações. Semelhante ao seno hiperbólico que oscila entre os extremos, eles são capazes de se mover suavemente entre diferentes abordagens e perspectivas."
     },
     {
         name: "Tangente Inversa Hiperbólica",
         type: "Carismático Cativante",
         points: 49,
+        characters: [
+            {
+              name: "Tony Stark",
+              origin: "Marvel",
+              imgUrl: "https://static.wikia.nocookie.net/marvel-cinematic/images/4/4b/Vingadores_Pôster_crop_Homem_de_Ferro.png/revision/latest?cb=20180518001633&path-prefix=pt-br"
+            },
+            {
+              name: "Barney Stinson",
+              origin: "How I Met Your Mother",
+              imgUrl: "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/01/24/1751375204-neil-patrick-harris-007.jpg"
+            },
+            {
+              name: "Harvey Specter",
+              origin: "Suits",
+              imgUrl: "https://static.wikia.nocookie.net/suits/images/d/da/S8_Harvey_Specter.jpg/revision/latest?cb=20180731195449"
+            }
+        ],
         desc: "Esta personalidade é carismática, persuasiva e tem uma capacidade única de atrair os outros para seu ponto de vista. Como a tangente inversa hiperbólica que descreve o crescimento lento e depois rápido, eles são capazes de criar um impacto duradouro com sua presença magnética."
     },
     {
         name: "Cosseno Inverso Hiperbólico",
         type: "Pragmático Determinado",
         points: 56,
+        characters: [
+            {
+              name: "Walter White",
+              origin: "Breaking Bad",
+              imgUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Walter_White_S5B.png/220px-Walter_White_S5B.png"
+            },
+            {
+              name: "Tommy Shelby",
+              origin: "Peaky Blinders",
+              imgUrl: "https://upload.wikimedia.org/wikipedia/pt/6/62/TommyShelby.jpg"
+            },
+            {
+              name: "Cersei Lannister",
+              origin: "Game of Thrones",
+              imgUrl: "https://static.wikia.nocookie.net/gameofthrones/images/2/2a/Cersei-queen.png/revision/latest?cb=20160725184739&path-prefix=pt-br"
+            }
+        ],
         desc: "Esse tipo de personalidade é prático, determinado e focado em alcançar seus objetivos. Assim como o cosseno inverso hiperbólico que descreve um crescimento inicial rápido seguido de uma desaceleração, eles têm uma abordagem metódica e calculada para alcançar o sucesso."
     },
     {
         name: "Secante Inversa Hiperbólica",
         type: "Harmonioso Conciliador",
         points: 64,
+        characters: [
+            {
+              name: "Gandalf",
+              origin: "Senhor dos Anéis",
+              imgUrl: "https://static.wikia.nocookie.net/liberproeliis/images/c/ce/E92fc6937ca21c5bb3311cfd137483c0.jpg/revision/latest?cb=20200522031537&path-prefix=pt-br"
+            },
+            {
+              name: "Tio Iroh",
+              origin: "Avatar: O Último Dobrador de Ar",
+              imgUrl: "https://static.wikia.nocookie.net/avatar/images/c/c1/Iroh_smiling.png/revision/latest/thumbnail/width/360/height/360?cb=20171117222711&path-prefix=pt-br                  "
+            },
+            {
+              name: "Marge Simpson",
+              origin: "The Simpsons",
+              imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFOoomhYsdCuWq7KZefzJRe-aXc8-X3qQUU9GDXTD22A&s"
+            }
+        ],
         desc: "Essa personalidade é pacífica, diplomática e sempre busca o equilíbrio e a harmonia nas relações. Como a secante inversa hiperbólica que descreve um crescimento inicial lento seguido de um aumento rápido, eles são capazes de encontrar soluções criativas e satisfatórias para conflitos e desafios interpessoais."
     }
 ]
